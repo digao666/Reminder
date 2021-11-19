15f6e47dc1b5e6c6ec83022104890c19f19ff717
d986000777 <dgao12@my.bcit.ca>
2021-11-19 14:39:22 -0800

    update friend_controller

--- frontend/controller/friend_controller.js
+++ frontend/controller/friend_controller.js
@@ -4,7 +4,7 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let friendsController = {
   list: (req, res) => {
     userid = req.user.id
-    
+
     let friends = []
     api = `http://localhost:8080/friends/${userid}`
         function Get(api){
@@ -13,34 +13,31 @@ let friendsController = {
             getfriends.send(null);
             return getfriends.responseText;
         }
-    let friendid = JSON.parse(Get(api))[0];
-
-    friendid.forEach(friends => {
-      let friend = getUserById(friendid)
-
-      friends.push({
-        email: friend.email,
-        amount: friend.reminders.length,
-        profilePic: friend.profilePic
-      })
+        let friendslist = JSON.parse(Get(api));
+        friendslist.forEach(obj => {
+            let friend = getUserById(obj.frn_friend_user_id)
+            friends.push({
+                email: friend.email,
+                amount: friend.reminders.length,
+                profilePic: friend.profilePic
+              })
     })
 
 
     let nonFriends = []
-    api = `http://localhost:8080/auth/user/all`
+    api = `http://localhost:8080/auth/users`
     function Get(api){
         const getnonfriends = new XMLHttpRequest();
         getnonfriends.open("GET",api,false);
         getonfriends.send(null);
         return getnonfrends.responseText;
     }
-
     let userlist = JSON.parse(Get(api))[0];
 
-    userlist.forEach( nonFriends => {
-      if (!(req.user.friends.includes(userlist)) && 
-      userlist !== req.user.id) {
-      let nonfriend = getUserById(userlist)
+    userlist.forEach( obj => {
+      if (!(req.user.friends.includes(obj.id)) && 
+      obj.id !== req.user.id) {
+      let nonfriend = getUserById(obj.id)
         nonFriends.push({
           email: nonfriend.email,
           amount: nonfriend.reminders.length,
--- frontend/controller/test.js
+++ frontend/controller/test.js
@@ -1,40 +1,47 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function add() {
-    let userid = 2
-    newFriend = 3
-    api = `http://localhost:8080/friends/${userid}`
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", api, true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        "user_id":userid,
-        "friend_id":newFriend
-      })
-      );
-}
-add()
-
-// function list() {
+// function add() {
 //     let userid = 1
+//     newFriend = 7
 //     api = `http://localhost:8080/friends/${userid}`
-//         function Get(api){
-//             const getfriends = new XMLHttpRequest();
-//             getfriends.open("GET",api,false);
-//             getfriends.send(null);
-//             return getfriends.responseText;
-//         }
-//     let friendid = JSON.parse(Get(api))[0];
-//         friendid.forEach(friends => {
-//         friend.email = "123"
-//         friend.reminders = [1,2,3]
-//         friend. profilePic = "123"
-//       friends.push({
-//         email: friend.email,
-//         amount: friend.reminders.length,
-//         profilePic: friend.profilePic
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("POST", api, true);
+//     xhr.setRequestHeader('Content-Type', 'application/json');
+//     xhr.send(JSON.stringify({
+//         "user_id":userid,
+//         "friend_id":newFriend
 //       })
-//     })
+//       );
 // }
+// add()
 
-// list()
+function list() {
+    let userid = 1
+    api = `http://localhost:8080/friends/${userid}`
+        function Get(api){
+            const getfriends = new XMLHttpRequest();
+            getfriends.open("GET",api,false);
+            getfriends.send(null);
+            return getfriends.responseText;
+        }
+    let friendslist = JSON.parse(Get(api));
+        friendslist.forEach(obj => {
+            let friend = getUserById(obj.frn_friend_user_id)
+            friends.push({
+                email: friend.email,
+                amount: friend.reminders.length,
+                profilePic: friend.profilePic
+              })
+    //     friend.email = "123"
+    //     friend.reminders = [1,2,3]
+    //     friend. profilePic = "123"
+    //   friends.push({
+    //     email: friend.email,
+    //     amount: friend.reminders.length,
+    //     profilePic: friend.profilePic
+    //   })
+    })
+    // console.log(friends)
+}
+
+list()
